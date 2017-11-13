08fda58b0e15369a10df92b64ad52b19f997032e
Sebastian Kürten <sebastian@topobyte.de>
2017-11-13 17:03:39 +0100

    Add a minimal test that crashes JavaParser

---
+++ src/test/java/de/topobyte/TestFailMinimal1.java
@@ -0,0 +1,37 @@
+package de.topobyte;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+
+import difflib.Delta;
+import difflib.DiffUtils;
+import difflib.Patch;
+
+public class TestFailMinimal1
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		String originalText = TestUtil.load("Fail.txt");
+		String targetText = TestUtil.load("Fail.mod.txt");
+
+		CompilationUnit originalCu = JavaParser.parse(originalText);
+		CompilationUnit cu = new ExternalizableRemover().transform(originalCu);
+		String modifiedText = LexicalPreservingPrinter.print(cu);
+
+		List<String> targetLines = TestUtil.lines(targetText);
+		List<String> modifiedLines = TestUtil.lines(modifiedText);
+
+		Patch<String> diffs = DiffUtils.diff(targetLines, modifiedLines);
+		for (Delta<String> delta : diffs.getDeltas()) {
+			System.out.println(delta.getType() + " " + delta);
+		}
+
+		System.out.println(targetText.equals(modifiedText));
+	}
+
+}
---
+++ src/test/java/de/topobyte/TestFailMinimal2.java
@@ -0,0 +1,37 @@
+package de.topobyte;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+
+public class TestFailMinimal2
+{
+
+	private static String text = "public class Foo {\n\n"
+			+ "// Some comment\n\n" // does work with only one \n
+			+ "public void writeExternal() {}\n" + "}";
+
+	public static void main(String[] args) throws IOException
+	{
+		CompilationUnit originalCu = JavaParser.parse(text);
+		CompilationUnit cu = LexicalPreservingPrinter.setup(originalCu);
+
+		cu.findAll(ClassOrInterfaceDeclaration.class).stream().forEach(c -> {
+			transform(c);
+		});
+	}
+
+	private static void transform(ClassOrInterfaceDeclaration c)
+	{
+		List<MethodDeclaration> methods = c.getMethodsByName("writeExternal");
+		for (MethodDeclaration method : methods) {
+			c.remove(method);
+		}
+	}
+
+}
