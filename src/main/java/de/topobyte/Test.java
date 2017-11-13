7ea15a8b01e64abec583b5c11a9bf49f61a9d803
Sebastian Kürten <sebastian@topobyte.de>
2017-11-13 10:45:15 +0100

    Test: remove some methods and rewrite file

--- src/main/java/de/topobyte/Test.java
+++ src/main/java/de/topobyte/Test.java
@@ -1,6 +1,7 @@
 package de.topobyte;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,7 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
 
 import de.topobyte.system.utils.SystemPaths;
 
@@ -18,16 +20,22 @@ public class Test
 	public static void main(String[] args) throws IOException
 	{
 		List<Path> files = new ArrayList<>();
+		files.add(SystemPaths.HOME
+				.resolve("github/OpenMetroMaps/OpenMetroMaps/java/maps-gwt/build/unpackedJars/com/slimjars/dist/gnu/trove/set/hash/TIntHashSet.java"));
 
-		Path file = SystemPaths.HOME
-				.resolve("github/OpenMetroMaps/OpenMetroMaps/java/maps-gwt/build/unpackedJars/com/slimjars/dist/gnu/trove/set/hash/TIntHashSet.java");
+		for (Path file : files) {
+			mod(file);
+		}
+	}
 
-		CompilationUnit cu = JavaParser.parse(file);
+	private static void mod(Path file) throws IOException
+	{
+		CompilationUnit originalCu = JavaParser.parse(file);
+		CompilationUnit cu = LexicalPreservingPrinter.setup(originalCu);
 
 		cu.findAll(ClassOrInterfaceDeclaration.class)
 				.stream()
-				.filter(c -> !c.isInterface()
-						&& !c.getName().getId().startsWith("Abstract"))
+				.filter(c -> !c.isInterface())
 				.forEach(
 						c -> {
 							System.out.println(c.getName());
@@ -36,12 +44,23 @@ public class Test
 									.getMethodsByName("writeExternal");
 							System.out.println("found writeExternal: "
 									+ writeExternal.size());
+							for (MethodDeclaration decl : writeExternal) {
+								boolean done = c.remove(decl);
+								System.out.println(done);
+							}
 
 							List<MethodDeclaration> readExternal = c
 									.getMethodsByName("readExternal");
 							System.out.println("found readExternal: "
 									+ readExternal.size());
+							for (MethodDeclaration decl : readExternal) {
+								boolean done = c.remove(decl);
+								System.out.println(done);
+							}
 						});
+
+		String text = LexicalPreservingPrinter.print(cu);
+		Files.write(file, text.getBytes());
 	}
 
 }
